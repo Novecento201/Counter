7d7e2b06eb9b2cbe2897cf203ee6d01333c2635a
Marco Lovato <marcolovatowork@gmail.com>
2022-02-09 21:58:49 +0100

    Correction for exercise, pass basic requests from html to js (counter, btn +, bnt -)

--- assets/index.js
+++ assets/index.js
@@ -1,6 +1,33 @@
 
 // Features required for the project
 
+// - Implementation of : counter and  buttons + and - 
+
+let containerFeatures = document.getElementById("container-features");
+
+let pCounter = document.createElement("p");
+pCounter.setAttribute("id","counter");
+pCounter.setAttribute("class","counter");
+
+let containerButtons = document.createElement("div");
+containerButtons.setAttribute("class","basic__buttons__counter");
+
+containerFeatures.appendChild(pCounter);
+containerFeatures.appendChild(containerButtons);
+
+let btnAdd = document.createElement("button");
+btnAdd.innerHTML= "+"
+btnAdd.setAttribute("id","incrase-one");
+btnAdd.setAttribute("class","button__counter button");
+
+let btnSub = document.createElement("button");
+btnSub.innerHTML= "-"
+btnSub.setAttribute("id","subtract-one");
+btnSub.setAttribute("class","button__counter button");
+
+containerButtons.appendChild(btnAdd);
+containerButtons.appendChild(btnSub);
+
 let counter = document.getElementById("counter");
 
 let btnIncraseOne = document.getElementById("incrase-one");
@@ -24,8 +51,8 @@ function subtractOne (){
     counter.innerHTML = count 
 };
 
-// Additional features at will
 
+// Additional features at will
 
 // Show additional content
 
